5988753842a80fe09fb137ece7bc8960106e49bb
Anthony Mai <anthony.mai530@gmail.com>
2020-02-10 02:57:46 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,2 +1,9 @@
-class CreateShows
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    create_table do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+  end
 end
